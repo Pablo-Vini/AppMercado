b3bfae88b23986b1c15f9d4e883095bfa0eb3ee8
Pablo-Vini <pablovgramos@gmail.com>
2023-08-24 21:36:54 -0300

    Criando Projeto Spring e Angular

---
+++ Java_Angular/src/main/java/Controler/Controle.java
@@ -0,0 +1,14 @@
+package Controler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controle {
+	
+	@GetMapping("/")
+	public String teste() {
+		return "Hello World"; 
+	}
+
+}
---
+++ Java_Angular/src/test/java/com/example/Java_Angular/JavaAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Java_Angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
